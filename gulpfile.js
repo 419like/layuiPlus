5dde3fd50cc2cd4cc675aae385335db14dec54ce
zhang419 <419like@gmail.com>
2017-06-08 09:45:30 +0800

    ignore node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ var gulp = require('gulp');
 var useref = require('gulp-useref');
 var gulpif = require('gulp-if');
 var del = require('del');
-var mkdirp = require('mkdirp');
+var vinylPaths = require('vinyl-paths');
 
 gulp.task('default', function() {
     // 将你的默认的任务代码放在这
     console.log('build----------发布项目');
+    gulp.run('build')
 });
 
 
@@ -17,18 +18,14 @@ gulp.task('html', function() {
 });
 
 gulp.task('copy', function() {
-    mkdirp('dist/lib', function(err) {
-        if (err) console.error(err)
-        else console.log('pow!')
-    });
     gulp.src('lib/jquery.min.js')
         .pipe(gulp.dest('dist/lib'));
 });
 
-gulp.task('clean', function(cb) {
-    del([
-        'dist/**/*',
-    ], cb);
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(gulp.dest('dist'))
+        .pipe(vinylPaths(del));
 });
 
 gulp.task('build', ['clean', 'copy', 'html']);
